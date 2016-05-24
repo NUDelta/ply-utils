09e9ef85206e8e2e2e99fa09f27264a0718196f3
Sarah Lim <me@sarahlim.com>
2016-05-24 12:02:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+import request from 'request';
+import fs from 'fs-extra';
+import path from 'path';
+import byline from 'byline';
+import {List, fromJS, forEach} from 'immutable';
+
+const src = path.join(__dirname, 'input');
+const dest = path.join(__dirname, 'output');
+
+// Ensure the output directory exists, otherwise create
+fs.ensureDir(dest, (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  // Walk over the input directory.
+  // Each filename represents a technique, and the
+  // contents are a list of urls to query and parse.
+  const techniques = fromJS(fs.readdirSync(src));
+
+  techniques.forEach((technique, key) => {
+    // Stream the file contents line by line,
+    // processing each line as a url.
+    const stream = byline(fs.createReadStream(
+      path.join(src, technique),
+      { encoding: 'utf8' })
+    );
+
+    let counter = -1;
+    let output = '';
+
+    stream.on('data', (url) => {
+      console.log('Processing', url);
+
+      counter += 1;
+      output = `${technique}-${counter}`;
+
+      // Get the html body
+      request
+        .get(url)
+        .on('error', (err) => {
+          console.log(err)
+        })
+        .pipe(fs.createWriteStream(
+          path.join(dest, output)
+        ));
+    });
+  });
+});
---
+++ src/getPropCounts.js
@@ -0,0 +1,52 @@
+const blocks = Array.from(document.querySelectorAll('pre'));
+const contents = blocks.map((el) => el.textContent);
+
+/* [<pre>] -> Void
+ * Displays snippets on page
+ */
+const displaySnippets = (snippets) => {
+    let results = document.createElement('div');
+    results.className = 'results';
+    
+    // Strip each snippet down to plaintext
+    snippets.forEach(function (pre) {
+        let stripped = document.createElement('pre');
+        stripped.textContent = pre.textContent;
+        results.appendChild(stripped);
+    });
+
+    // Replace the contents of the document body with the matched snippets
+    let body = document.getElementsByTagName('body')[0];
+    body.innerHTML = '';
+    body.appendChild(results);
+}
+
+/* [String], [String] -> {String: Number}
+ * Takes a list of snippet contents and a list of keywords
+ * Counts the total number of times each keyword appears over all of the snippets
+ * Returns a dictionary of keyword: count pairs
+ */
+export function countKeywords (contents, keywords) {
+    let results = {};
+    contents.forEach((str) => {
+        // Tokenize the content string by splitting along whitespace
+        const tokens = str.split(/\s+/);
+
+        tokens
+            // Tokens ending in ':' are likely to be CSS properties
+            .filter((tk) => tk.charAt(tk.length - 1) === ':')
+            // Extract the property and remove false positives by checking against keyword bank
+            .map((tk) => tk.slice(0, tk.length - 1))
+            .filter((tk) => keywords.indexOf(tk) !== -1)
+            // Update the appropriate entry in the dictionary
+            .forEach((tk) => {
+                if (tk in results) {
+                    results[tk] += 1;
+                } else {
+                    results[tk] = 1;
+                }
+            });
+    });
+
+    return results;
+}
